d968285909fa9f13354e8d972ca3f27c7bc33477
Ethan Perez <perez@nyu.edu>
2021-06-10 15:26:18 -0700

    Fix import

--- scripts/rda_utils.py
+++ scripts/rda_utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-from tqdm.auto import tqdm
+from tqdm import tqdm
 import pathlib
 import re
 from scipy.stats import multivariate_normal
